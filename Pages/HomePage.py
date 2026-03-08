ec9d9f4492189f738ea3a5f814d1dd2959b4b16b
Milosz Skorupski <skorupskimilosz@gmail.com>
2026-03-08 02:57:35 +0100

    dodanie lokatorw do home page

--- Pages/HomePage.py
+++ Pages/HomePage.py
@@ -4,15 +4,28 @@ from Pages.BasePage import BasePage
 
 
 class HomePage(BasePage):
-    # LOCAKALIZATORY
+    # LOKALIZATORY
 
     URL = "https://automationintesting.online/"
 
-    BOOK_NOW = (By.XPATH, "//a[contains(@href, '#booking')]")
-    CHECK_AVAILABILITY = (By.XPATH, "//a[contains(@class,'openBooking')]")
-    CHECK_IN = (By.XPATH, "//input[@id='checkin']")
-    CHECK_OUT = (By.XPATH, "//input[@id='checkout']")
-    WELCOME_TEXT = (By.XPATH, "//p[contains(@class,'lead')]")
+    BOOK_NOW = (By.XPATH, '//a[contains(@href, "#booking")]')
+    CHECK_AVAILABILITY = (By.XPATH, '//a[contains(@class, "openBooking")]')
+    CHECK_IN = (By.XPATH, '//input[@id="checkin"]')
+    CHECK_OUT = (By.XPATH, '//input[@id="checkout"]')
+    WELCOME_TEXT = (By.XPATH, '//p[contains(@class, "lead")]')
+    Footer_text = (By.XPATH, '//*[@id="root-container"]/div/nav/div/a')
+    Footer_rooms_Nav_link = (By.XPATH, '//a[contains(@href, "#rooms")]')
+    Footer_booking_Nav_link = (By.XPATH, '//a[contains(@href, "#booking")]')
+    Footer_Amenities_Nav_link = (By.XPATH, '//*[@id="navbarNav"]/ul/li[3]/a]')
+    Footer_Location_Nav_link = (By.XPATH, '//*[@id="navbarNav"]/ul/li[4]/a]')
+    Footer_Contact_Nav_link = (By.XPATH, '//*[@id="navbarNav"]/ul/li[5]/a')
+    Footer_Admin_Nav_link = (By.XPATH, '//*[@id="navbarNav"]/ul/li[6]/a')
+    Send_Us_A_Message_Name =(By.XPATH, '//*[@id="name"]')
+    Send_Us_A_Message_Email = (By.XPATH, '//*[@id="email"]')
+    Send_Us_A_Message_Phone = (By.XPATH, '//*[@id="phone"]')
+    Send_Us_A_Message_Subject = (By.XPATH, '//*[@id="subject"]')
+    Send_Us_A_Message_Message = (By.XPATH, '//*[@id="message"]')
+    Send_Us_A_Message_Submit_Button = (By.XPATH, '//*[@id="contact"]/div/div/div/div/div/form/div[6]/button')
 
     # METODY
 
@@ -22,11 +35,7 @@ class HomePage(BasePage):
     def click_check_availability_to_booking(self):
         self.click(self.CHECK_AVAILABILITY)
 
-    # Metoda sprawdzająca czy tekst się nie zmienił
     def get_text(self, locator):
-        # Znajdujemy element na stronie
         element = self.driver.find_element(*locator)
-        # Sprawdzamy, czy jest widoczny
         assert element.is_displayed()
-        # Czekamy aż Selenium potwierdzi widoczność i zwracamy tekst
         return self.wait.until(EC.visibility_of_element_located(locator)).text
