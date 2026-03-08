59455ec7105405884ede14578e3ce93ea7adc635
Milosz Skorupski <skorupskimilosz@gmail.com>
2026-03-08 14:03:02 +0100

    dodanie lokatorw do home page

--- Pages/HomePage.py
+++ Pages/HomePage.py
@@ -1,6 +1,6 @@
+from Pages.BasePage import BasePage
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-from Pages.BasePage import BasePage
 
 
 class HomePage(BasePage):
@@ -26,16 +26,20 @@ class HomePage(BasePage):
     Send_Us_A_Message_Subject = (By.XPATH, '//*[@id="subject"]')
     Send_Us_A_Message_Message = (By.XPATH, '//*[@id="message"]')
     Send_Us_A_Message_Submit_Button = (By.XPATH, '//*[@id="contact"]/div/div/div/div/div/form/div[6]/button')
+    Contact_Information_HOTEL_Address = (By.XPATH, '//*[@id="location"]/div/div[2]/div[2]/div/div/div[1]/div[2]/p')
+    Contact_Information_HOTEL_Phone = (By.XPATH, '//*[@id="location"]/div/div[2]/div[2]/div/div/div[2]/div[2]/p')
+    Contact_Information_HOTEL_Email = (By.XPATH, '//*[@id="location"]/div/div[2]/div[2]/div/div/div[3]/div[2]/p')
+
 
     # METODY
 
     def open(self):
         self.driver.get(self.URL)
+        return self
 
     def click_check_availability_to_booking(self):
         self.click(self.CHECK_AVAILABILITY)
 
     def get_text(self, locator):
-        element = self.driver.find_element(*locator)
-        assert element.is_displayed()
-        return self.wait.until(EC.visibility_of_element_located(locator)).text
+        element = self.wait.until(EC.visibility_of_element_located(locator))
+        return element.text
